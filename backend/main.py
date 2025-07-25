e62a706e15d95c324a8aa59ef17192aab679b7c5
Shams <shamsrami2004@gmail.com>
2025-07-25 19:14:59 +0300

    Database connection and testing

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,21 @@
 from fastapi import FastAPI
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from fastapi import Depends
+from dependencies import get_db
 
 app = FastAPI()
 
+# API test
 @app.get("/")
 def root():
     return {"message":"HourglassEd API test"}
+
+## database connection test
+@app.get("/db-test")
+def test_db(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+        return {"status" : "success"}
+    except Exception as e:
+        return {"status": "error", "detail": str(e)}
