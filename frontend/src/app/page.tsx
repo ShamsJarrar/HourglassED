657b0e581c4e4f01e7f777044311f9352b622a46
Shams <shamsrami2004@gmail.com>
2025-08-17 16:56:00 +0300

    Restarting frontend

--- frontend/src/app/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation';
-
-export default function HomePage() {
-  // Redirect to calendar page - this will be the main landing page for logged-in users
-  redirect('/calendar');
-}
